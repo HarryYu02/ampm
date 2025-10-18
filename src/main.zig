9b9044bb9b779de245586423044c2b25025a1803
Harry Yu <yupakhei456@gmail.com>
2025-10-17 22:44:51 -0400

    basic command parsing

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,40 @@
-const std = @import("std");
 const ampm = @import("ampm");
+const builtin = @import("builtin");
+const std = @import("std");
+const process = std.process;
+const mem = std.mem;
+
+const Command = enum {
+    install,
+    uninstall,
+};
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try ampm.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    switch (builtin.target.os.tag) {
+        .macos => {
+            std.debug.print("All your {s} are belong to us.\n", .{"Mac"});
+        },
+        else => {
+            std.debug.print("OS not supported.\n", .{});
+            return;
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    }
+
+    var command: Command = undefined;
+    var args_iter = process.args();
+    _ = args_iter.next();
+    const command_arg = args_iter.next();
+    if (command_arg == null) {
+        std.debug.print("No command provided.\n", .{});
+        return;
+    } else if (mem.eql(u8, command_arg.?, "install")) {
+        command = Command.install;
+        std.debug.print("Install\n", .{});
+    } else if (mem.eql(u8, command_arg.?, "uninstall")) {
+        command = Command.uninstall;
+        std.debug.print("Uninstall\n", .{});
+    } else {
+        std.debug.print("Unknown command provided.\n", .{});
+        return;
+    }
 }
